43729e9fcecd9491205ba3887761122131805fda
Diya Gandhi <diyajeon141@gmail.com>
2026-02-26 19:55:19 +0530

    Update script.js

--- java/script.js
+++ java/script.js
@@ -1,1 +1,19 @@
-console.log("day1 Task LOaded")
+
+console.log("day1 Task loaded");
+
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+const body = document.body;
+const navLinks = document.querySelectorAll('.nav-link a');
+
+hamburger.addEventListener('click', () => {
+    body.classList.toggle('nav-open');
+    body.classList.toggle('no-scroll');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        body.classList.remove('nav-open');
+        body.classList.remove('no-scroll');
+    });
+});
